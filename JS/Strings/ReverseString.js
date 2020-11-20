8bd6550295c4e5fff0f081365ac3fcf73118b584
Oleksandra <karska.dev@gmail.com>
2020-11-19 17:44:56 -0800

    Update Reverse String problem with possible variants from real interview

--- JS/Strings/ReverseString.js
+++ JS/Strings/ReverseString.js
@@ -1,18 +1,49 @@
+// Reverse string (first split it to array, reverse and than join it to string back) using additional char variable
 var reverseString = function(s) {
+  if (s === '') return s;
+  s = s.split('');
   let ch = '';
   for (let i = 0; i < Math.floor(s.length/2); i++) {
       ch = s[s.length - 1 - i];
       s[s.length - 1 - i] = s[i];
       s[i] = ch;
   }
-  return s;
+  return s.join('');
 };
 
+// Reverse string (first split it to array, reverse and than join it to string back) using ES6 swipe possibility
 var reverseString2 = function(s) {
+  if (s === '') return s;
+  s = s.split('');
   for (let i = 0; i < Math.floor(s.length/2); i++) {
       [s[i], s[s.length - 1 - i]] = [s[s.length - 1 - i], s[i]];
   }
-  return s;
+  return s.join('');
 };
 
-console.log(["h","e","l","l","o"]);
+// Reverse each word in the sentence but not the sentence
+function reverseSentence(str) {
+  if (str === '') return str;
+  let sentence = str.split(' ');
+
+  for (let i = 0; i < sentence.length; i++) {
+    sentence[i] = reverseString(sentence[i]);
+  }
+
+  return sentence.join(' ');
+}
+
+// Reverse sentence, but not the words
+function reverseSentence1(str) {
+  if (str === '') return str;
+  let sentence = str.split(' ');
+
+  sentence = sentence.reverse();
+
+  return sentence.join(' ');
+}
+
+// console.log(reverseString('hello')); // olleh
+// console.log(reverseString2('world')); // dlrow
+// console.log(reverseSentence('hello world')); // olleh dlrow
+// console.log(reverseSentence1('hello world')); // world hello
